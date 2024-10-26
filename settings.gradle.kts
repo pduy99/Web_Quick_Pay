4a1ffb3a18a46720040be6366bf974e329c22d40
pduy99 <phuocduypham2502@gmail.com>
2024-10-26 19:39:41 +0700

    Init project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.helios.web.quickpay"
---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.cio.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
